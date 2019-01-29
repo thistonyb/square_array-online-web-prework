833049113973fd1830ccd97a414426f960e70c52
Tony Baker <this.tonybaker@gmail.com>
2019-01-29 20:49:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   #squared_array = []
   array.each do |number|
     squared_number = number**2
-    squared_array.push(squared_number)
+    squared_array=[].push(squared_number)
   end
   return squared_array
 end
